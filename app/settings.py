d2dd90f784e638d193f28b869b8e20efd590052a
Isaias Santos de Miranda <isaias.miranda@revemarnet.com.br>
2024-06-05 19:13:48 -0300

    config uwfgi params static_root env config

--- app/settings.py
+++ app/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-f5!4s_ec)7@4x0ty7nlkr&oqckl-6mh+@h94zyrtgfr8(b^zc3
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['10.1.33.209']
+ALLOWED_HOSTS = ['10.1.33.142']
 
 
 # Application definition
@@ -126,6 +126,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
